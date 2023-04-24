63ed7197163b15e8f830176414602ffd4ddf5843
nicoleavieir4 <nicoleavieira@icloud.com>
2023-04-24 10:43:39 -0300

    novos exercicios

--- JavaScript/ex009/fila.js
+++ JavaScript/ex009/fila.js
@@ -1,9 +1,23 @@
 const prompt = require("prompt-sync")();
 
 const pacientes = ["Rita", " Marco"];
+let fila = "";
 
-const msg = prompt(`Olá, paciente! Nesse momento temos as seguintes pessoas na fila de espera: ${pacientes}`);
+const msg = prompt(
+  `Olá, paciente! Nesse momento temos as seguintes pessoas na fila de espera: ${pacientes}`
+);
 console.log(msg);
+
+function position(fila) {
+  if (fila === "1" || fila === "2") {
+    for (let i = 0; i < pacientes.length; i) {
+      const elemento = pacientes[i];
+      console.log(
+        "O(a) paciente " + elemento + " encontra-se na posição " + ++i
+      );
+    }
+  }
+}
 
 do {
   fila = prompt(
@@ -19,20 +33,14 @@ do {
       console.log(
         `O paciente ${adicionar} foi adicionado a lista de espera. Agora esses são os próximos pacientes: ${pacientes}`
       );
-      if (fila === "1" || fila === "2") {
-        for (let i = 0; i < pacientes.length; i) {
-          const elemento = pacientes[i];
-          console.log(
-            "O(a) paciente " + elemento + " encontra-se na posição " + ++i
-          );
-        }
-      }
+      position(fila);
       break;
     case "2":
       const consultarPaciente = pacientes.shift();
       console.log(
         `Você atendeu o paciente ${consultarPaciente}. Agora esses são os próximos pacientes: ${pacientes}`
       );
+      position(fila);
       break;
     case "3":
       console.log("Você escolheu sair. O programa será encerrado!");
