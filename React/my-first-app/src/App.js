a39598602ec994b64eb16ea5be78a452f3475800
nicoleavieir4 <nicoleavieira@icloud.com>
2023-06-05 12:50:41 -0300

    upgrade

---
+++ React/my-first-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function HelloWorld() {
+  return <h1>Hello World</h1>;
+}
+
+function App() {
+  return (
+    <div>
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
