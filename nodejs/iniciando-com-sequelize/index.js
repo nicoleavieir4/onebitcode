4a58d7d7cd76233c9d51ccc9ed318b916e5ce7e2
nicoleavieir4 <nicoleavieira@icloud.com>
2023-06-19 17:55:17 -0300

    update

--- nodejs/iniciando-com-sequelize/index.js
+++ nodejs/iniciando-com-sequelize/index.js
@@ -1,3 +1,15 @@
+const express = require("express");
+const routes = require("./routes");
+
+require("../config/associations");
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3000);
+
 (async () => {
   const Planet = require("./models/Planet");
 
