381966a7cfc744b7ca7796d091a9372bc9ab0e65
nicoleavieir4 <nicoleavieira@icloud.com>
2023-06-19 14:41:46 -0300

    integrando postgresql

--- nodejs/iniciando-com-express/app.js
+++ nodejs/iniciando-com-express/app.js
@@ -8,9 +8,9 @@ require("./config/database");
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}))
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.set("views", path.join(__dirname, "src/views"));
 app.set("view engine", "ejs");
---
+++ nodejs/iniciando-com-sequelize/index.js
@@ -0,0 +1,26 @@
+(async () => {
+  const Planet = require("./models/Planet");
+
+  const newPlanet = await Planet.create({
+    name: "Terra",
+    position: 3,
+  });
+
+  const seePlanets = await Planet.findAll(); // consulta todos planetas cadastrados no banco de dados
+  // const seePlanets = await Planet.findByPk(1); // cosulta por id
+
+  // const seePlanets = await Planet.findAll({
+  //   where: {
+  //     name: "Terra",
+  //   },
+  // }); // consulta todos planetas cadastrados com o nome "Terra" no banco de dados
+
+  const updatePlanets = await Planet.findByPk(1);
+  updatePlanets.name = "Venus";
+  await updatePlanets.save();
+
+  const deletePlanets = await Planet.findByPk(1);
+
+  console.log(deletePlanets);
+  await deletePlanets.destroy();
+})();
